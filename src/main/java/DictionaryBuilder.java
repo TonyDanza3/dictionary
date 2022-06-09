a70e1bec32e3d56249c33a6b951e3f27dd99bb3c
TonyDanza3 <you@example.com>
2022-06-09 12:57:20 +0300

    implemented count dictionary data methods

--- src/main/java/DictionaryBuilder.java
+++ src/main/java/DictionaryBuilder.java
@@ -1,29 +1,50 @@
+import com.jayway.jsonpath.*;
+import net.minidev.json.JSONObject;
 
-import java.util.HashMap;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public  class DictionaryBuilder {
 
+    String dictionary;
+    public  void readJsonDictionary() throws Exception {
+        String file = "src/main/java/dictionary.json";
+        dictionary = readFileAsString(file);
+    }
+    public static String readFileAsString(String file)throws Exception
+    {
+        return new String(Files.readAllBytes(Paths.get(file)));
+    }
 
 
 
-    public static void returnDictionary(){
 
-
-        //countSections
+    public void returnDictionary(){
+        try {
+            readJsonDictionary();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        int sectionsCount = countSections();
         //countwords
+        int[] wordsCount = countWords();
         //countTranslations
         //
     }
+
     public int countSections (){
         int sections = 0;
-
+        sections = JsonPath.read(dictionary,"$['sections'].size()");
         return sections;
     }
 
-    public int countWords (String section){
-        int words = 0;
-
-        return words;
+    public int[] countWords (){
+        int sectionsCount = JsonPath.read(dictionary,"$['sections'].size()");
+        int [] wordsCount = new int[sectionsCount];
+        for(int i=0;i < sectionsCount; i++){
+            wordsCount[i] = JsonPath.read(dictionary,"$['sections'][" + i + "].['content'].size()");
+        }
+        return wordsCount;
     }
 
     public int countTranslations(String word){
@@ -31,9 +52,4 @@ public  class DictionaryBuilder {
 
         return translations;
     }
-
-
-
-
-
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,43 +8,48 @@ public class Main {
     //TODO synonyms mode
 
     public static void main (String []args){
-        Checker checker = new Checker();
-        HashMap dictionary = DictionaryBuilder.returnDitionary();
-        WordsTable wordsTable = new WordsTable();
-        wordsTable.generageWordsTable(dictionary);
-        int dictionarySize = dictionary.size() - 1;
-        Scanner sc = new Scanner(System.in);
+        DictionaryBuilder dic = new DictionaryBuilder();
+        dic.returnDictionary();
 
-        boolean wordsIsOver = false;
-        while (!wordsIsOver){
-            int i = RandomGenerator.generateIntFromZeroTo(dictionarySize);
-            while (wordsTable.getWordMark(i)==true){
-                i = RandomGenerator.generateIntFromZeroTo(dictionarySize);
-                if(!checker.remainingWords(wordsTable)){
-                    break;
-                }
-            }
 
-            String rusWord = (String) wordsTable.getRusWord(i);
-            String engWord = (String) wordsTable.getEngWord(i);
-            System.out.print(rusWord + ": ");
-            String userInput =sc.nextLine();
-            boolean isRight =checker.checkTranslation(userInput,engWord);
-            if(!isRight){
-                System.out.print("\u274C");
-            }
-            if(isRight){
-                System.out.print("\u2713");
-            }
-            wordsTable.setWordMark(i);
-            if(!checker.remainingWords(wordsTable)){
-                wordsIsOver = true;
-                System.out.println("");
-                System.out.println("That's all the words from dictionary. Exiting =)");
-            }
-            System.out.println("");
 
-        }
 
+//        Checker checker = new Checker();
+//        HashMap dictionary = DictionaryBuilder.returnDitionary();
+//        WordsTable wordsTable = new WordsTable();
+//        wordsTable.generageWordsTable(dictionary);
+//        int dictionarySize = dictionary.size() - 1;
+//        Scanner sc = new Scanner(System.in);
+//
+//        boolean wordsIsOver = false;
+//        while (!wordsIsOver){
+//            int i = RandomGenerator.generateIntFromZeroTo(dictionarySize);
+//            while (wordsTable.getWordMark(i)==true){
+//                i = RandomGenerator.generateIntFromZeroTo(dictionarySize);
+//                if(!checker.remainingWords(wordsTable)){
+//                    break;
+//                }
+//            }
+//
+//            String rusWord = (String) wordsTable.getRusWord(i);
+//            String engWord = (String) wordsTable.getEngWord(i);
+//            System.out.print(rusWord + ": ");
+//            String userInput =sc.nextLine();
+//            boolean isRight =checker.checkTranslation(userInput,engWord);
+//            if(!isRight){
+//                System.out.print("\u274C");
+//            }
+//            if(isRight){
+//                System.out.print("\u2713");
+//            }
+//            wordsTable.setWordMark(i);
+//            if(!checker.remainingWords(wordsTable)){
+//                wordsIsOver = true;
+//                System.out.println("");
+//                System.out.println("That's all the words from dictionary. Exiting =)");
+//            }
+//            System.out.println("");
+//
+//        }
     }
 }
